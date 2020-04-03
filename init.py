4ea8fb985aa198c2b4dfea26a928b862982ac3e6
Brian Robinson <bigrobinson@gmail.com>
2020-04-03 10:30:58 -0500

    Update call to xavier_uniform to xavier_uniform_
    Update call from deprecated name for Pytorch 1.4

--- init.py
+++ init.py
@@ -4,7 +4,7 @@ import torch.nn.init as init
 def xavier_uniform_relu(modules):
     for m in modules:
         if isinstance(m, nn.Conv2d):
-            init.xavier_uniform(m.weight.data, gain=init.calculate_gain('relu'))
+            init.xavier_uniform_(m.weight.data, gain=init.calculate_gain('relu'))
             if m.bias is not None:
                 m.bias.data.zero_()
         elif isinstance(m, nn.BatchNorm2d):
@@ -14,7 +14,7 @@ def xavier_uniform_relu(modules):
 def xavier_uniform_sigmoid(modules):
     for m in modules:
         if isinstance(m, nn.Conv2d):
-            init.xavier_uniform(m.weight.data, gain=init.calculate_gain('sigmoid'))
+            init.xavier_uniform_(m.weight.data, gain=init.calculate_gain('sigmoid'))
             if m.bias is not None:
                 m.bias.data.zero_()
         elif isinstance(m, nn.BatchNorm2d):
